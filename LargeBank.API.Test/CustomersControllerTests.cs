e7e52c9af4dc98ec654d44701c7b94eb2052b83c
jeffward01 <jeffwardcoding@gmail.com>
2015-10-15 14:51:19 -0700

    added first test

---
+++ LargeBank.API.Test/CustomersControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LargeBank.API.Controllers;
+using System.Collections.Generic;
+using LargeBank.API.Models;
+using System.Linq;
+
+namespace LargeBank.API.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetCustomersReturnsCustomers()
+        {
+            //Arrange
+            //Properties go here
+            var customerController = new CustomersController();
+
+            //Act
+            //Call the method in question that needs to be tested
+            IEnumerable<CustomerModel> customers = customerController.GetCustomers();
+
+
+            //Assert
+            //You assert that if the outcome is this, assert that.
+            Assert.IsTrue(customers.Count() > 0);
+        }
+
+
+    }
+}
